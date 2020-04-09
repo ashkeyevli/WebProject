26482829a3719b1cdb6d3af76885d02c59c24952
Ashkeyev Ali <ashkeyevli@gmail.com>
2020-04-10 05:39:32 +0600

    order functionality

--- front/src/app/model.ts
+++ front/src/app/model.ts
@@ -2,13 +2,21 @@ export interface IDish {
   id: number;
   name: string;
   price: number;
-  image_url: string;
+  imageUrl: string;
   description: string;
   menu: number;
+  count: number;
 
 }
 export interface IMenu {
   id: number;
   name: string;
   image_url_menu: string;
+}
+export interface IOrder {
+id: number;
+name: string;
+imageUrl: string;
+price: number;
+count: number;
 }
